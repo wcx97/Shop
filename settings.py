bfb76deef00c715580efa5fd461437a09f899f16
wcx97 <48108077+wcx97@users.noreply.github.com>
2022-10-28 03:25:24 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,17 +2,17 @@ from pydantic import BaseSettings
 
 
 class _Settings(BaseSettings):
-    TOKEN: str = ''
-    GUILD: int = ''
+    TOKEN: str = 'MTAzNTQ1MjYxMDk2ODMwNTY5NA.GnKeaE.7Hlk-_kfbxv42Y7QXqEW9m2OVeriSNMC2V46DA'
+    GUILD: int = '1031438146795216896'
     TIMEZONE: str = 'Asia/Shanghai'
     DEBUG: bool = False
     DATABASE: str = 'sqlite+aiosqlite:///database.db'
     # bot工作的channel ID
-    CHANNEL_ID: int = 1026003196050673751
+    CHANNEL_ID: int = 1031438146795216899
     # 管理员channel ID
-    ADMIN_CHANNEL_ID: int = 1026003196050673751
+    ADMIN_CHANNEL_ID: int = 1031438146795216899
     # 生日祝福类channel
-    BIRTHDAY_CHANNEL: int = 1026003196050673751
+    BIRTHDAY_CHANNEL: int = 1031438146795216899
 
     GIVEAWAY_MAX_MINUTES: int = 60  # 设置最大的抽奖分钟数
 
